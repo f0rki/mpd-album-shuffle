8af02ca158f6c4fb07d51f4556445c17389e4fe9
Michael Rodler <michael@michaelrodler.at>
2013-01-27 15:51:38 +0100

    cleanup according to pep8

--- mpd-album-shuffle.py
+++ mpd-album-shuffle.py
@@ -16,7 +16,8 @@
 #
 
 
-import sys, os
+import sys
+import os
 import argparse
 import random
 import logging
@@ -24,6 +25,7 @@ import mpd
 
 
 MPD_CONNECTION_TIMEOUT = 10
+
 
 class mpd_connect(object):
     """
@@ -46,7 +48,7 @@ class mpd_connect(object):
         try:
             self.client.connect(self.host, self.port)
         except ConnectionRefusedError as e:
-            logging.error("Couldn't connect to MPD at {}:{} ({})"\
+            logging.error("Couldn't connect to MPD at {}:{} ({})"
                     .format(self.host, self.port, e))
             return None
         if self.password is not None:
@@ -55,7 +57,8 @@ class mpd_connect(object):
             except mpd.CommandError as e:
                 logging.error("MPD didn't accept the password")
                 return None
-        logging.debug("connected to MPD version: {}".format(self.client.mpd_version))
+        logging.debug("connected to MPD version: {}"
+                .format(self.client.mpd_version))
         return self.client
 
     def __exit__(self, type, value, traceback):
@@ -64,10 +67,11 @@ class mpd_connect(object):
             self.client.disconnect()
 
 
-
 class AlbumList(object):
-    """Class which wraps functionality to maintain a list of all albums in the
-    current playlist"""
+    """
+    Class which wraps functionality to maintain a list of all albums in the
+    current playlist
+    """
 
     def __init__(self, client):
         self.client = client
@@ -86,7 +90,7 @@ class AlbumList(object):
             else:
                 #TODO: every song without album info should be considered to be
                 # in a separate album
-                logging.debug("no album information, ignoring entry: {}"\
+                logging.debug("no album information, ignoring entry: {}"
                         .format(song))
 
     def find_album_boundaries(self, albumname):
@@ -97,14 +101,14 @@ class AlbumList(object):
         if entries:
             return int(entries[0]["pos"]), int(entries[-1]["pos"])
         else:
-            return None, None #TODO: or raise Exception?
+            return None, None  # TODO: or raise Exception?
 
     def get_current_album(self):
         return self.client.currentsong()["album"]
 
     def choose_random_album(self, current):
-        """picks a random album from the current playlist, while trying to avoid
-        the current album.
+        """picks a random album from the current playlist, while trying to
+        avoid the current album.
         """
         if self.albums:
             if len(self.albums) == 1:
@@ -117,7 +121,6 @@ class AlbumList(object):
         else:
             logging.warn("No albums found")
             return None
-
 
     def play_random(self):
         """Play first song of a random album in the current playlist."""
@@ -136,8 +139,9 @@ class AlbumList(object):
             logging.error("Playlist seems to be empty")
 
     def shuffle_albums(self):
-        """Essentially do a random.shuffle over all albums and sort the playlist
-        accordingly. This assumes that the playlist is already sorted by albums.
+        """Essentially do a random.shuffle over all albums and sort the
+        playlist accordingly. This assumes that the playlist is already
+        sorted by albums.
         """
         logging.info("shuffling albums")
         albumlist = list(self.albums)
@@ -148,8 +152,8 @@ class AlbumList(object):
             if first is None or last is None:
                 logging.error("No boundaries for album {}".format(album))
             else:
-                logging.debug("moving {}:{} to position 0".format(first,last))
-                songrange = "{}:{}".format(first, last+1)
+                logging.debug("moving {}:{} to position 0".format(first, last))
+                songrange = "{}:{}".format(first, last + 1)
                 self.client.move(songrange, 0)
 
 
@@ -159,7 +163,7 @@ def extract_mpd_credentials(string, defaults=None):
     host, port, pw = None, None, None
     if defaults is not None:
         host, port, pw = defaults[0], defaults[1], defaults[2]
-    logging.debug("{} {} {}".format(host,port,pw))
+    logging.debug("{} {} {}".format(host, port, pw))
     if string:
         if "@" in string:
             pw, con = string.split("@")
@@ -170,22 +174,22 @@ def extract_mpd_credentials(string, defaults=None):
                 host, port = con.split(":")
             else:
                 host = con
-    logging.debug("{} {} {}".format(host,port,pw))
+    logging.debug("{} {} {}".format(host, port, pw))
     return host, port, pw
 
 
 def main():
-    parser = argparse.ArgumentParser(\
-            description="Randomly play or sort the albums in the current mpd playlist")
-    parser.add_argument("-v", "--verbose", action="store_true", \
+    parser = argparse.ArgumentParser(description=
+            "Randomly play or sort the albums in the current mpd playlist")
+    parser.add_argument("-v", "--verbose", action="store_true",
             help="increase verbosity of output")
-    parser.add_argument("-vv", dest="debug", action="store_true", \
+    parser.add_argument("-vv", dest="debug", action="store_true",
             help="turn on debug output (really really verbose)")
-    parser.add_argument("-p", "--port", nargs=1, type=int,\
+    parser.add_argument("-p", "--port", nargs=1, type=int,
             help="the port to connect to")
-    parser.add_argument("-H", "--host", nargs=1,\
+    parser.add_argument("-H", "--host", nargs=1,
             help="the hostname to connect to")
-    parser.add_argument("-P", "--password", nargs=1,\
+    parser.add_argument("-P", "--password", nargs=1,
             help="the password")
     parser.add_argument("command", choices=["play", "sort"])
     args = parser.parse_args(sys.argv[1:])
@@ -195,9 +199,9 @@ def main():
         loglevel = logging.DEBUG
         fmt = "%(levelname)s:%(name)s - %(message)s"
     elif args.verbose:
-        loglevel=logging.INFO
+        loglevel = logging.INFO
     else:
-        loglevel=logging.WARN
+        loglevel = logging.WARN
     logging.basicConfig(level=loglevel, format=fmt)
 
     # fetch mpd credentials
@@ -205,14 +209,15 @@ def main():
     mpdenv = os.getenv("MPD_HOST")
     if mpdenv:
         logging.debug("found MPD_HOST in env {}".format(mpdenv))
-        host, port, pw = extract_mpd_credentials(mpdenv, (host,port,pw))
+        host, port, pw = extract_mpd_credentials(mpdenv, (host, port, pw))
     if args.port:
         port = args.port[0]
     if args.password:
         pw = args.password[0]
     if args.host:
         host = args.host[0]
-    logging.debug("connecting to host: {} port: {} pw: {}".format(host, port, pw))
+    logging.debug("connecting to host: {} port: {} pw: {}"
+            .format(host, port, pw))
 
     try:
         with mpd_connect(host, port, pw) as client:
